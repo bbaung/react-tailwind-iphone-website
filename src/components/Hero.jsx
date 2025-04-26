520408476a037ad3edfd223ac4e1307d68e892a9
arjun <arjunvisibleone.arjun@gmail.com>
2025-04-26 13:18:39 +0630

    first commit

---
+++ src/components/Hero.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Hero = () => {
+    return(
+        <>
+            <h1 className="text-white">Hero section</h1>
+
+        </>
+    )
+}
+
+export default Hero;
---
+++ src/components/Highlights.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Highlights = () => {
+    return(
+        <>
+            <h1 className="text-white">Navbar</h1>
+        </>
+    )
+}
+
+export default Highlights;
