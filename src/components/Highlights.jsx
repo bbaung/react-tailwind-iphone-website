ae1f31866d705c16f79c5e44b9dd4785ab0aec31
arjun <arjunvisibleone.arjun@gmail.com>
2025-05-31 12:49:20 +0630

    add: videoCarousel

--- src/components/Highlights.jsx
+++ src/components/Highlights.jsx
@@ -1,10 +1,43 @@
 import React from "react";
+import gasp from "gsap";
+import { useGSAP } from "@gsap/react";
+import { watchImg } from "../utils/index.js";
+import { rightImg } from "../utils/index.js";
+import VideoCarousel from "./VideoCarousel.jsx";
 
 const Highlights = () => {
+
+    useGSAP( () => {
+        gasp.to("#title",{ opacity: 0.5, y: 0, delay: 1.5})
+        gasp.to(".link",{opacity: 1, y:0, delay: 1.7, stagger: 0.25})
+    })
+
     return(
-        <>
-            <h1 className="text-white">Hight light</h1>
-        </>
+        <section id="hightlights" className="w-screen-overflow-hidden sm:py-32 py-20 sm:px-10 px-5 h-full bg-[#37303050]">
+            <div className="scrim-max-width">
+                <div className="mb-12 w-full flex items-end justify-between">
+                    <h1 id="title" className="text-gray-100 lg:text-6xl md:text-5xl text-3xl lg:mb-0 mb-5 font-medium opacity-0 translate-y-20">
+                        Get the hightlights.
+                    </h1>
+
+                    <div className="flex flex-wrap items-end gap-5">
+                        <p className="text-white link hover:underline cursor-pointer flex items-center text-xl opacity-0 translate-y-20">
+                            Watch the film
+                            <img className="ml-2" src={watchImg} alt="watch" />
+                        </p>
+
+                        <p className="text-white link hover:underline cursor-pointer flex items-center text-xl opacity-0 translate-y-20">
+                            Watch the event
+                            <img className="ml-2" src={rightImg} alt="right" />
+                        </p>
+                    </div>
+                </div>
+
+                <VideoCarousel/>
+            </div>
+
+        
+        </section>
     )
 }
 
---
+++ src/components/VideoCarousel.jsx
@@ -0,0 +1,55 @@
+import { hightlightsSlides } from "../constants";
+import { useState , useEffect , useRef } from "react";
+
+const VideoCarousel = () => {
+
+    const videoRef = useRef([]);
+    const videoSpanRef = useRef([]);
+    const videoDivRef = useRef([]);
+
+    const [video , setVideo] = useState({
+        isEnd : false,
+        startPlay : false,
+        videoId : 0,
+        isLastVideo : false,
+        isPlaying : false
+    })
+
+    const { isEnd , startPlay, videoId,  isLastddVideo , isPlaying} = video;
+
+    useEffect( () =>{
+
+    })
+
+    return (
+        <>
+
+            <div className="flex items-center overflow-hidden">
+                {hightlightsSlides.map(list => (
+                    <div key={list.id} id="slider" className=" sm:pr-20 pr-10">
+                        <div className="relative sm:w-[70vw] w-[88vw] md:h-[70vh] sm:h-[50vh] h-[35vh]">
+                            <div className=" w-full h-full flex items-center justify-center rounded-3xl overflow-hidden bg-black">
+                                <video
+                                    id="video"
+                                    playsInline={true}
+                                    preload="auto"
+                                    muted
+                                >
+                                    <source src={list.video} type="video/mp4" />
+                                </video>
+                            </div>
+
+                            <div className=" absolute top-12 left-[5%]">
+                                {list.textLists.map((items,i)=>(
+                                    <p key={i}>{items}</p>
+                                ))}
+                            </div>
+                        </div>
+                    </div>
+                ))}
+            </div>
+        </>
+    )
+}
+
+export default VideoCarousel;
